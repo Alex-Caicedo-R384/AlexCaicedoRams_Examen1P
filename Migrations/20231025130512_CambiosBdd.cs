c59d5a6f10bdab5fa8233c717e4ae63ffa70b703
Alex Caicedo Ramos <alex.caicedo@udla.edu.ec>
2023-10-25 08:09:18 -0500

    3er Commit Se implemento las migraciones del sistema en la base de Datos y se creo la base de datos desde la consola de administración con el nombre del estudiante.

---
+++ Migrations/20231025130512_CambiosBdd.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AlexCaicedoRams_Examen1P.Migrations
+{
+    public partial class CambiosBdd : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AlexCaicedo_Tabla",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AcCedula = table.Column<int>(type: "int", maxLength: 12, nullable: false),
+                    AcNombre = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    AcApellido = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    AcMayordeEdad = table.Column<bool>(type: "bit", nullable: false),
+                    AcIngresos = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    AcFechaNacimiento = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AlexCaicedo_Tabla", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AlexCaicedo_Tabla");
+        }
+    }
+}
