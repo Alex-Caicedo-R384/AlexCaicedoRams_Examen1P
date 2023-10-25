e8b3179aa80c9b31dc902c00406319851d1f2509
Alex Caicedo Ramos <alex.caicedo@udla.edu.ec>
2023-10-25 08:51:38 -0500

    6to Commit Se edito el nombre del display de los atributos para que quedara mejor a la vista y no causara confucion al momento de crear,ver,eliminar una lista.

--- Models/AlexCaicedo_Tabla.cs
+++ Models/AlexCaicedo_Tabla.cs
@@ -9,26 +9,32 @@ namespace AlexCaicedoRams_Examen1P.Models
         [RegularExpression("(^[0-9]+$)", ErrorMessage = "Solo se permiten números")]
         [Required(AllowEmptyStrings = false, ErrorMessage = "El número es obligatorio")]
         [Range(10, int.MaxValue, ErrorMessage = "El número máximo es de 10 dgitos")]
-
-
+        [Display(Name = "Cédula")]
         public int? AcCedula { get; set; }
 
         [Required(AllowEmptyStrings = false, ErrorMessage = "El nombre es obligatorio")]
         [StringLength(50)]
+        [Display(Name = "Nombre")]
+
         public string? AcNombre { get; set; }
 
         [Required(AllowEmptyStrings = false, ErrorMessage = "El Apellido es obligatorio")]
         [StringLength(50)]
+        [Display(Name = "Apellido")]
+
         public string? AcApellido { get; set; }
 
         [Required(AllowEmptyStrings = false, ErrorMessage = "La validación es obligatorio")]
+        [Display(Name = "Mayor de Edad")]
         public bool AcMayordeEdad { get; set; }
 
         [Range(450.00, 5072.99)]
+        [Display(Name = "Ingresos")]
         public decimal AcIngresos { get; set; }
 
         [Required(AllowEmptyStrings = false, ErrorMessage = "La Fecha de Nacimiento es obligatoria")]
-        [DataType(DataType.DateTime)]
+        [DataType(DataType.Date)]
+        [Display(Name = "Fecha de Nacimiento")]
         public DateTime? AcFechaNacimiento { get; set; }
 
     }
